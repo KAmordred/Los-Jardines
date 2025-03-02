4e8f4ea960a1bf547a96dab68ce47cb49ea97b0f
KAmordred <alangabriel49@gmail.com>
2025-03-02 19:21:18 -0300

    comienzos

---
+++ script.js
@@ -0,0 +1,6 @@
+// Función para mostrar un mensaje en la consola
+console.log("¡Bienvenido al Club!");
+
+document.addEventListener("DOMContentLoaded", function() {
+    alert("¡Bienvenido al sitio web de Los-Jardines!");
+});
